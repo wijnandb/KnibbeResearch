abbdad1a7ee99663bae868f00efddbcebc292b6d
Elise <koster.elise@gmail.com>
2014-11-20 17:34:00 +0100

    Start of re-design KRA website

--- js/modern-business.js
+++ js/modern-business.js
@@ -1,9 +1,22 @@
 // Activates the Carousel
 $('.carousel').carousel({
   interval: 5000
-})
+});
 
 // Activates Tooltips for Social Links
 $('.tooltip-social').tooltip({
   selector: "a[data-toggle=tooltip]"
+});
+
+$(document).ready(function(){	
+	$('#membership-dialog').dialog({
+		autoOpen: false,
+		height: 500,
+		width: 500,
+		dialogClass: 'readmore-dialog'
+	});	
+
+	$('#open-membership').click(function(){
+		$('#membership-dialog').dialog("open");
+	});
 })
