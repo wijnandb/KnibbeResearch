127ae369317d6832e0c1d59df2e1afb148d08768
Elise <koster.elise@gmail.com>
2014-11-27 12:29:42 +0100

    Almost done, final tweaks

--- js/modern-business.js
+++ js/modern-business.js
@@ -19,4 +19,48 @@ $(document).ready(function(){
 	$('#open-membership').click(function(){
 		$('#membership-dialog').dialog("open");
 	});
+
+	$('#real-estate-dialog').dialog({
+		autoOpen: false,
+		height: 200,
+		width: 500,
+		dialogClass: 'readmore-dialog'
+	});	
+
+	$('#open-real-estate').click(function(){
+		$('#real-estate-dialog').dialog("open");
+	});
+
+	$('#investors-dialog').dialog({
+		autoOpen: false,
+		height: 200,
+		width: 500,
+		dialogClass: 'readmore-dialog'
+	});	
+
+	$('#open-investors').click(function(){
+		$('#investors-dialog').dialog("open");
+	});
+
+	$('#banks-dialog').dialog({
+		autoOpen: false,
+		height: 200,
+		width: 500,
+		dialogClass: 'readmore-dialog'
+	});	
+
+	$('#open-banks').click(function(){
+		$('#banks-dialog').dialog("open");
+	});
+
+	$('#consultancy-dialog').dialog({
+		autoOpen: false,
+		height: 550,
+		width: 500,
+		dialogClass: 'readmore-dialog'
+	});	
+
+	$('#open-consultancy').click(function(){
+		$('#consultancy-dialog').dialog("open");
+	});
 })
